bfafe87c8e37391228149ceb86a051abcfd61deb
Craig <craigmiller160@gmail.com>
2021-12-29 14:11:12 -0500

    Improved eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,11 @@ module.exports = {
     extends: [
         'eslint:recommended'
     ],
+    env: {
+        browser: true,
+        amd: true,
+        node: true,
+    },
     rules: {
         'no-console': [
             'error',
