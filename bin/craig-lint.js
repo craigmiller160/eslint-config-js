8a3017c04c6cb6bb9c7f6f4cca12c82e0d241443
Craig Miller <craigmiller160@gmail.com>
2022-01-02 14:46:09 -0500

    Improved script exit code

--- bin/craig-lint.js
+++ bin/craig-lint.js
@@ -2,6 +2,8 @@
 
 const spawn = require('cross-spawn');
 
-spawn.sync('eslint', ['{src/**/**.{js,jsx,ts,tsx},test/**/**.{js,jsx,ts,tsx}}', '--fix', '--max-warnings=0'], {
+const result = spawn.sync('eslint', ['{src/**/**.{js,jsx,ts,tsx},test/**/**.{js,jsx,ts,tsx}}', '--fix', '--max-warnings=0'], {
     stdio: 'inherit'
 });
+
+process.exit(result.status);
